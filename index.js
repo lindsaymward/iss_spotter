915ca7009e5903f70ac2cb2480d8d358bf1c2318
Lindsay Ward <lward23@hotmail.com>
2023-05-11 15:51:18 -0700

    Implement fetch IP function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Require and run main fetch function
+
+const { fetchMyIP } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
+
--- iss.js
+++ iss.js
@@ -1,1 +1,25 @@
 // Contain most logic for fetching data from API endpoints.
+const request = require('request');
+
+const fetchMyIP = (callback) => {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    body = JSON.parse(body);
+    const ip = body.ip;
+    callback(null, ip);
+  });
+};
+
+
+module.exports = { fetchMyIP };
+
+// 'https://api.ipify.org?format=json'
